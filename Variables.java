e8ee1fa212e25e21cdbcbb87520bf3d4660647af
Tomasz Grabarczyk <32244889+tomasz-grabarczyk@users.noreply.github.com>
2019-04-14 22:48:07 +0200

    Deleted unused variables

--- Variables.java
+++ Variables.java
@@ -12,16 +12,12 @@ class Variables {
     JButton submit = new JButton("Log in");
     JButton backUp = new JButton("Back");
 
-    private String[] userAndPasswordArray = {"admin", "password", "tgrabarczyk@nzl.pl", "tgpass", "mjazdzewska@nzl.pl", "mjpass", "kturski@nzl.pl", "ktpass"};
-    private String[] nieZnanyLekarzDoctors = {"Internista", "Onkolog", "Pediatra", "Ginekolog"};
+    private String[] userAndPasswordArray = {"admin", "123", "tgrabarczyk@nzl.pl", "tgpass", "mjazdzewska@nzl.pl", "mjpass", "kturski@nzl.pl", "ktpass"};
+    private String[] nieZnanyLekarzDoctors = {"Internista", "Onkolog", "Pediatra", "Ginekolog", "Kardiolog", "Ortopeda", "Urolog"};
 
     List<String> userAndPassword = new ArrayList<>(Arrays.asList(userAndPasswordArray));
     List<String> nieZnanyLekarzDoctorsList = new ArrayList<>(Arrays.asList(nieZnanyLekarzDoctors));
 
     JButton[] numberOfDoc = new JButton[nieZnanyLekarzDoctorsList.size()];
-    JFrame nieZnanyLekarzInternista = new JFrame("Internista");
-    JFrame nieZnanyLekarzOnkolog = new JFrame("Onkolog");
-    JFrame nieZnanyLekarzPediatra = new JFrame("Pediatra");
-    JFrame nieZnanyLekarzGinekolog = new JFrame("Ginekolog");
-
+    JFrame[] nieZnanyLekarzSpecjalista = new JFrame[nieZnanyLekarzDoctorsList.size()];
 }
