71d061231a93462aa9894d1140303d17e14ec4bb
Tomasz Grabarczyk <32244889+tomasz-grabarczyk@users.noreply.github.com>
2019-04-14 21:05:18 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        DoctorAppointment doctorAppointment = new DoctorAppointment();
+        doctorAppointment.showLoginScreen();
+    }
+}
