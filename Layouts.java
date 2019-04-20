e655342d33ecfa39faae39b68ddff1b59b2d1280
tomasz-grabarczyk <tomasz.gra@interia.pl>
2019-04-20 22:36:20 +0200

    Done refactoring, added new layout for buttons for doctors.

--- Layouts.java
+++ Layouts.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 
-
 class Layouts extends Variables {
 
     private void setLayout(JFrame frame, int width, int height, boolean resize) {
@@ -25,12 +24,11 @@ class Layouts extends Variables {
     }
 
     private void setLayoutNieZnanyLekarzFrame(JFrame nieZnanyLekarzFrame) {
-        backUp.setBounds(10, 10, 100, 50);
+        backUp.setBounds(10, 130, 100, 50);
         backUp.setVisible(true);
         backUp.addActionListener(e1 -> setLayoutSecondWindow());
 
-        doctorLabel.setBounds(10,100,100,100);
-        doctorLabel.setVisible(true);
+        setLayoutInternista();
 
         nieZnanyLekarzFrame.setLayout(null);
         nieZnanyLekarzFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,34 +41,45 @@ class Layouts extends Variables {
     private void setLayoutFirstWindow() {
         setLayout(firstWindow, 470, 130, false);
     }
+
     void setLayoutSecondWindow() {
         setLayout(selectDoctorSpeciality, 335, 70 * nieZnanyLekarzDoctorsList.size() + 45, false);
-
 
         int y = 10;
 
         for (int numOfDoc = 0; numOfDoc < nieZnanyLekarzDoctorsList.size(); numOfDoc++) {
             nieZnanyLekarzSpecjalista[numOfDoc] = new JFrame(nieZnanyLekarzDoctorsList.get(numOfDoc));
-            nieZnanyLekarzSpecjalista[0].add(doctorLabel);
 
             numberOfDoc[numOfDoc] = new JButton();
             numberOfDoc[numOfDoc].setBounds(10, y, 300, 60);
             numberOfDoc[numOfDoc].setVisible(true);
             numberOfDoc[numOfDoc].setText(nieZnanyLekarzDoctorsList.get(numOfDoc));
+            selectDoctorSpeciality.add(numberOfDoc[numOfDoc]);
+
             int finalNumOfDoc = numOfDoc;
             numberOfDoc[numOfDoc].addActionListener(e -> {
                 selectDoctorSpeciality.dispose();
+                System.out.println(numberOfDoc[finalNumOfDoc].getText()); // print name of doctor to console
                 setLayoutNieZnanyLekarzFrame(nieZnanyLekarzSpecjalista[finalNumOfDoc]);
             });
-            selectDoctorSpeciality.add(numberOfDoc[numOfDoc]);
-            y = y + 70;
-        }
 
+            y += 70;
+        }
     }
 
     void setLayoutMessageWindow() {
         setLayout(messageWindow, 300, 100, false);
     }
 
+    private void setLayoutInternista() {
+        doctorLabel.setBounds(10,170,100,100);
+        doctorLabel.setVisible(true);
+        JButton btnDoc = new JButton();
+        btnDoc.setIcon(imageIcon);
+        btnDoc.setBounds(10,10,100,100);
+        btnDoc.setVisible(true);
+        nieZnanyLekarzSpecjalista[0].add(btnDoc);
+        nieZnanyLekarzSpecjalista[0].add(doctorLabel);
 
+    }
 }
